6d03d443c491896baefa8d0778fe94c5a9e1c72a
war-boy <ryan.eutsler@outlook.com>
2020-10-26 19:27:08 -0500

    First commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+﻿var currentDate = new Date();
+
+var currentYear = currentDate.getFullYear();
+
+var copyright = document.getElementById('copyright')
+
+copyright.textContent = currentYear;
+
+$(function () {
+    $(".thumb").on("click", function () {
+        var imgSrc = $(this).attr("src");
+
+        $("#lightbox-container").html('<img src="' + imgSrc + '" class="img-responsive" />');
+        $("#lightbox-container").fadeIn(500);
+        $("#lightbox-container").on("click", function () {
+            $(this).fadeOut(500);
+        });
+
+    });
+});
